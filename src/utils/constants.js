2541d119689501c1c42e4306853ba7cb013174fb
Dmytro <just.another.dev.acc@gmail.com>
2025-06-11 21:15:06 +0300

    Wrap constant objects with Object.freeze

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,15 +4,15 @@ export const MAX_TESTS = 11;
 export const BOARD_SIZE = 19;
 export const WIN_STREAK = 5;
 
-export const Stone = {
+export const Stone = Object.freeze({
   Empty: 0,
   Black: 1,
   White: 2,
-};
+});
 
-export const DIRECTIONS = [
-  { row: 0, column: 1 },
-  { row: 1, column: 0 },
-  { row: 1, column: 1 },
-  { row: 1, column: -1 },
-];
+export const DIRECTIONS = Object.freeze([
+  Object.freeze({ row: 0, column: 1 }),
+  Object.freeze({ row: 1, column: 0 }),
+  Object.freeze({ row: 1, column: 1 }),
+  Object.freeze({ row: 1, column: -1 }),
+]);

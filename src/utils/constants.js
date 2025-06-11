582944c2ce01836062c9bd474560ad884bb99bc5
Dmytro <just.another.dev.acc@gmail.com>
2025-06-11 16:40:49 +0300

    added WIN_STREAK, DIRECTIONS

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,9 +2,17 @@ export const MIN_TESTS = 1;
 export const MAX_TESTS = 11;
 
 export const BOARD_SIZE = 19;
+export const WIN_STREAK = 5;
 
 export const Stone = {
   Empty: 0,
   Black: 1,
   White: 2,
 };
+
+export const DIRECTIONS = [
+  { row: 0, column: 1 },
+  { row: 1, column: 0 },
+  { row: 1, column: 1 },
+  { row: 1, column: -1 },
+];
